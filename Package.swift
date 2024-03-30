28b81a9f7c366125b6b8abc10086b48ceb46bef1
Sereivoan Yong <sereivoanyong@gmail.com>
2024-03-31 03:35:11 +0700

    Bump swift-tools-version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
   name: "ScannerUIKit",
   platforms: [
-    .iOS(.v11)
+    .iOS(.v12)
   ],
   products: [
     .library(name: "ScannerUIKit", targets: ["ScannerUIKit"])
